32c322cc2cc40051774d3f8e4e8f6e8e7ed15992
platocrat <jack.abe.winfield@gmail.com>
2021-08-25 00:05:53 -0700

    1st attempt at mappings for LogTake, LogMake, LogKill

--- src/mappings/rubiconMarket.ts
+++ src/mappings/rubiconMarket.ts
@@ -1,152 +1,216 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
   RubiconMarket,
-  FeeTake,
-  LogBump,
-  LogBuyEnabled,
-  LogDelete,
-  LogInsert,
-  LogInt,
-  LogItemUpdate,
+  // FeeTake,
+  // LogBump,
+  // LogBuyEnabled,
+  // LogDelete,
+  // LogInsert,
+  // LogInt,
+  // LogItemUpdate,
   LogKill,
   LogMake,
-  LogMatch,
-  LogMatchingEnabled,
-  LogMinSell,
-  LogNote,
-  LogSetAuthority,
-  LogSetOwner,
-  LogSortedOffer,
+  // LogMatch,
+  // LogMatchingEnabled,
+  // LogMinSell,
+  // LogNote,
+  // LogSetAuthority,
+  // LogSetOwner,
+  // LogSortedOffer,
   LogTake,
-  LogTrade,
-  LogUnsortedOffer,
-  OfferDeleted
-} from "../generated/RubiconMarket/RubiconMarket"
-import { } from "../../generated/schema/"
+  // LogTrade,
+  // LogUnsortedOffer,
+  // OfferDeleted
+} from "../../generated/RubiconMarket/RubiconMarket"
+import {
+  LogKill as LogKillEntity,
+  LogMake as LogMakeEntity,
+  LogTake as LogTakeEntity
+} from "../../generated/schema"
 
-export function handleFeeTake(event: FeeTake): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+// export function ExampleEntity(event: ExampleEntity): void {
+//   // Entities can be loaded from the store using a string ID; this ID
+//   // needs to be unique across all entities of the same type
+//   let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+//   // Entities only exist after they have been saved to the store;
+//   // `null` checks allow to create entities on demand
+//   if (entity == null) {
+//     entity = new ExampleEntity(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+//     // Entity fields can be set using simple assignments
+//     entity.count = BigInt.fromI32(0)
+//   }
+
+//   // BigInt and BigDecimal math are supported
+//   entity.count = entity.count + BigInt.fromI32(1)
+
+//   // Entity fields can be set based on event parameters
+//   entity.id = event.params.id
+//   entity.pair = event.params.pair
+
+//   // Entities can be written to the store with `.save()`
+//   entity.save()
+
+//   // Note: If a handler doesn't require existing field values, it is faster
+//   // _not_ to load the entity from the store. Instead, create it fresh with
+//   // `new Entity(...)`, set the fields that should be updated and save the
+//   // entity back to the store. Fields that were not set or unset remain
+//   // unchanged, allowing for partial updates to be applied.
+
+//   // It is also possible to access smart contracts from mappings. For
+//   // example, the contract that has emitted the event can be connected to
+//   // with:
+//   //
+//   // let contract = Contract.bind(event.address)
+//   //
+//   // The following functions can then be called on this contract to access
+//   // state variables and other data:
+//   //
+//   // - contract.AqueductAddress(...)
+//   // - contract.AqueductDistributionLive(...)
+//   // - contract._best(...)
+//   // - contract._dust(...)
+//   // - contract._head(...)
+//   // - contract._near(...)
+//   // - contract._rank(...)
+//   // - contract._span(...)
+//   // - contract.buy(...)
+//   // - contract.buyAllAmount(...)
+//   // - contract.buyEnabled(...)
+//   // - contract.cancel(...)
+//   // - contract.del_rank(...)
+//   // - contract.dustId(...)
+//   // - contract.getBestOffer(...)
+//   // - contract.getBetterOffer(...)
+//   // - contract.getBuyAmount(...)
+//   // - contract.getFirstUnsortedOffer(...)
+//   // - contract.getMinSell(...)
+//   // - contract.getNextUnsortedOffer(...)
+//   // - contract.getOffer(...)
+//   // - contract.getOfferCount(...)
+//   // - contract.getOwner(...)
+//   // - contract.getPayAmount(...)
+//   // - contract.getTime(...)
+//   // - contract.getWorseOffer(...)
+//   // - contract.initialized(...)
+//   // - contract.insert(...)
+//   // - contract.isActive(...)
+//   // - contract.isClosed(...)
+//   // - contract.isOfferSorted(...)
+//   // - contract.last_offer_id(...)
+//   // - contract.make(...)
+//   // - contract.matchingEnabled(...)
+//   // - contract.offer(...)
+//   // - contract.offer(...)
+//   // - contract.offer(...)
+//   // - contract.offers(...)
+//   // - contract.owner(...)
+//   // - contract.sellAllAmount(...)
+//   // - contract.setAqueductAddress(...)
+//   // - contract.setAqueductDistributionLive(...)
+//   // - contract.setBuyEnabled(...)
+//   // - contract.setFeeBPS(...)
+//   // - contract.setFeeTo(...)
+//   // - contract.setMatchingEnabled(...)
+//   // - contract.setMinSell(...)
+//   // - contract.stopped(...)
+// }
+
+// export function handleLogBump(event: LogBump): void { }
+
+// export function handleLogBuyEnabled(event: LogBuyEnabled): void { }
+
+// export function handleLogDelete(event: LogDelete): void { }
+
+// export function handleLogInsert(event: LogInsert): void { }
+
+// export function handleLogInt(event: LogInt): void { }
+
+// export function handleLogItemUpdate(event: LogItemUpdate): void { }
+
+export function handleLogKill(event: LogKill): void {
+  let ep = event.params,
+    lkID = ep.id.toString(),
+    logKill = LogKillEntity.load(lkID)
+
+  // If no entity exists for this ID, we create it.
+  if (logKill == null) {
+    logKill = new LogKillEntity(lkID)
+  } else {
+    logKill.id = lkID
+    logKill.pair = ep.pair
+    logKill.maker = ep.maker
+    logKill.pay_gem = ep.pay_gem
+    logKill.buy_gem = ep.buy_gem
+    logKill.pay_amt = ep.pay_amt
+    logKill.buy_amt = ep.buy_amt
+    logKill.timestamp = ep.timestamp
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.id = event.params.id
-  entity.pair = event.params.pair
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.AqueductAddress(...)
-  // - contract.AqueductDistributionLive(...)
-  // - contract._best(...)
-  // - contract._dust(...)
-  // - contract._head(...)
-  // - contract._near(...)
-  // - contract._rank(...)
-  // - contract._span(...)
-  // - contract.buy(...)
-  // - contract.buyAllAmount(...)
-  // - contract.buyEnabled(...)
-  // - contract.cancel(...)
-  // - contract.del_rank(...)
-  // - contract.dustId(...)
-  // - contract.getBestOffer(...)
-  // - contract.getBetterOffer(...)
-  // - contract.getBuyAmount(...)
-  // - contract.getFirstUnsortedOffer(...)
-  // - contract.getMinSell(...)
-  // - contract.getNextUnsortedOffer(...)
-  // - contract.getOffer(...)
-  // - contract.getOfferCount(...)
-  // - contract.getOwner(...)
-  // - contract.getPayAmount(...)
-  // - contract.getTime(...)
-  // - contract.getWorseOffer(...)
-  // - contract.initialized(...)
-  // - contract.insert(...)
-  // - contract.isActive(...)
-  // - contract.isClosed(...)
-  // - contract.isOfferSorted(...)
-  // - contract.last_offer_id(...)
-  // - contract.make(...)
-  // - contract.matchingEnabled(...)
-  // - contract.offer(...)
-  // - contract.offer(...)
-  // - contract.offer(...)
-  // - contract.offers(...)
-  // - contract.owner(...)
-  // - contract.sellAllAmount(...)
-  // - contract.setAqueductAddress(...)
-  // - contract.setAqueductDistributionLive(...)
-  // - contract.setBuyEnabled(...)
-  // - contract.setFeeBPS(...)
-  // - contract.setFeeTo(...)
-  // - contract.setMatchingEnabled(...)
-  // - contract.setMinSell(...)
-  // - contract.stopped(...)
+  logKill.save()
 }
 
-export function handleLogBump(event: LogBump): void { }
+export function handleLogMake(event: LogMake): void {
+  let ep = event.params,
+    lmID = ep.id.toString(),
+    logMake = LogMakeEntity.load(lmID)
 
-export function handleLogBuyEnabled(event: LogBuyEnabled): void { }
+  // If no entity exists for this ID, we create it.
+  if (logMake == null) {
+    logMake = new LogMakeEntity(lmID)
+  } else {
+    logMake.id = lmID
+    logMake.pair = ep.pair
+    logMake.maker = ep.maker
+    logMake.pay_gem = ep.pay_gem
+    logMake.buy_gem = ep.buy_gem
+    logMake.pay_amt = ep.pay_amt
+    logMake.buy_amt = ep.buy_amt
+    logMake.timestamp = ep.timestamp
+  }
 
-export function handleLogDelete(event: LogDelete): void { }
+  logMake.save()
+}
 
-export function handleLogInsert(event: LogInsert): void { }
+// export function handleLogMatch(event: LogMatch): void { }
 
-export function handleLogInt(event: LogInt): void { }
+// export function handleLogMatchingEnabled(event: LogMatchingEnabled): void { }
 
-export function handleLogItemUpdate(event: LogItemUpdate): void { }
+// export function handleLogMinSell(event: LogMinSell): void { }
 
-export function handleLogKill(event: LogKill): void { }
+// export function handleLogNote(event: LogNote): void { }
 
-export function handleLogMake(event: LogMake): void { }
+// export function handleLogSetAuthority(event: LogSetAuthority): void { }
 
-export function handleLogMatch(event: LogMatch): void { }
+// export function handleLogSetOwner(event: LogSetOwner): void { }
 
-export function handleLogMatchingEnabled(event: LogMatchingEnabled): void { }
+// export function handleLogSortedOffer(event: LogSortedOffer): void { }
 
-export function handleLogMinSell(event: LogMinSell): void { }
+export function handleLogTake(event: LogTake): void {
+  let ep = event.params,
+    ltID = ep.id.toString(),
+    logTake = LogTakeEntity.load(ltID)
 
-export function handleLogNote(event: LogNote): void { }
+  // If no entity exists for this ID, we create it.
+  if (logTake == null) {
+    logTake = new LogTakeEntity(ltID)
+  } else {
+    logTake.id = ltID
+    logTake.pair = ep.pair
+    logTake.maker = ep.maker
+    logTake.pay_gem = ep.pay_gem
+    logTake.buy_gem = ep.buy_gem
+    logTake.take_amt = ep.take_amt
+    logTake.give_amt = ep.give_amt
+    logTake.timestamp = ep.timestamp
+  }
 
-export function handleLogSetAuthority(event: LogSetAuthority): void { }
+  logTake.save()
+}
 
-export function handleLogSetOwner(event: LogSetOwner): void { }
+// export function handleLogTrade(event: LogTrade): void { }
 
-export function handleLogSortedOffer(event: LogSortedOffer): void { }
+// export function handleLogUnsortedOffer(event: LogUnsortedOffer): void { }
 
-export function handleLogTake(event: LogTake): void { }
-
-export function handleLogTrade(event: LogTrade): void { }
-
-export function handleLogUnsortedOffer(event: LogUnsortedOffer): void { }
-
-export function handleOfferDeleted(event: OfferDeleted): void { }
+// export function handleOfferDeleted(event: OfferDeleted): void { }
